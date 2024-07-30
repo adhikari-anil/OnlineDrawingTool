f7cad5000198598c3bae6b0aa00008773f07425e
Anil Adhikari <adhikarianil719@gmail.com>
2024-07-30 17:34:12 +0545

    Test eventlisteners added for mobile browser

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,24 +7,24 @@ const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
-  const {activeMenuItem, actionMenuItem} = useSelector((state)=>state.menu);
-  const {color, size} = useSelector((state) => state.toolBox[activeMenuItem]);
-  
-  useEffect(()=>{
+  const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
+  const { color, size } = useSelector((state) => state.toolBox[activeMenuItem]);
+
+  useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    if(actionMenuItem === MENU_ITEMS.DOWNLOAD){
+    if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
       const URL = canvas.toDataURL();
       const anchor = document.createElement("a");
-      anchor.href =URL
-      anchor.download = 'sketch.jpg'
-      anchor.click()
+      anchor.href = URL;
+      anchor.download = "sketch.jpg";
+      anchor.click();
       console.log(URL);
     }
-    dispatch(actionItemClick(null))
-  },[actionMenuItem])
+    dispatch(actionItemClick(null));
+  }, [actionMenuItem]);
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -36,8 +36,8 @@ const Board = () => {
       context.lineWidth = size;
     };
 
-    changeConfig(color,size);
-  },[color,size]);
+    changeConfig(color, size);
+  }, [color, size]);
 
   useLayoutEffect(() => {
     if (!canvasRef.current) return;
@@ -48,40 +48,59 @@ const Board = () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const beginPath=(x,y)=>{
-      context.beginPath()
-      context.moveTo(x,y)
-    }
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
 
-    const drawLine = (x,y)=>{
-      context.lineTo(x,y)
-      context.stroke()
-    }
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
 
-    const handleMouseDown = (e)=>{
-        shouldDraw.current = true
-        beginPath(e.clientX, e.clientY);
-    }
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      beginPath(e.clientX, e.clientY);
+    };
 
-    const handleMouseMove = (e)=>{
-        if(!shouldDraw.current) return
-        drawLine(e.clientX, e.clientY)
-    }
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) return;
+      drawLine(e.clientX, e.clientY);
+    };
 
-    const handleMouseUp = (e)=>{
-        shouldDraw.current = false
-    }
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    const handleTouchStart = (e) => {
+      shouldDraw.current = true;
+      beginPath(e.clientX, e.clientY);
+    };
+
+    const handleTouchMove = (e) => {
+      if (!shouldDraw.current) return;
+      drawLine(e.clientX, e.clientY);
+    };
+
+    const handleTouchUp = (e) => {
+      shouldDraw.current = false;
+    };
 
     canvas.addEventListener("mousedown", handleMouseDown);
     canvas.addEventListener("mousemove", handleMouseMove);
     canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("touchstart", handleTouchStart);
+    canvas.addEventListener("touchmove", handleTouchMove);
+    canvas.addEventListener("touchend", handleTouchUp);
 
     return () => {
       canvas.removeEventListener("mousedown", handleMouseDown);
       canvas.removeEventListener("mousemove", handleMouseMove);
       canvas.removeEventListener("mouseup", handleMouseUp);
+      canvas.removeEventListener("touchstart", handleTouchStart);
+      canvas.removeEventListener("touchmove", handleTouchMove);
+      canvas.removeEventListener("touchend", handleTouchUp);
     };
-
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
